2c6ba956adafa259ac7c680b9d533349c91076ed
djpn <36060426+davidjpnelson@users.noreply.github.com>
2022-10-18 12:42:09 -0500

    Added more program control statements

--- src/HelpEx.java
+++ src/HelpEx.java
@@ -1,15 +1,27 @@
-import java.io.IOException;
+/*
 
+An improved Help system that uses a do-while to process menu selection.
+
+ */
+import java.io.IOException;
 public class HelpEx {
 
   public static void main(String[] args) throws IOException {
-    char choice;
-    System.out.println("Help on");
-    System.out.println("1. if");
-    System.out.println("2. switch");
-    System.out.println("Choose one:");
+    char choice, ignore;
+    do {
+      System.out.println("Help on");
+      System.out.println("1. if");
+      System.out.println("2. switch");
+      System.out.println("3. for");
+      System.out.println("4. while");
+      System.out.println("5. do-while\n");
+      System.out.println("Choose one:");
 
-    choice = (char) System.in.read();
+      choice = (char) System.in.read();
+      do {
+        ignore = (char) System.in.read();
+      } while (ignore != '\n');
+    } while (choice <'1' | choice > '5');
 
     System.out.println();
 
@@ -30,8 +42,23 @@ public class HelpEx {
         System.out.println("    statement sequence;");
         System.out.println("}");
       }
-      default ->
-        System.out.println("Selection not found.");
+      case '3' ->{
+        System.out.println("The for:\n");
+        System.out.println("for(init; condition; iteration)");
+        System.out.println("  statement;");
+      }
+      case '4' ->{
+        System.out.println("The while:\n");
+        System.out.println("while(condition) statement;");
+      }
+      case '5' ->{
+        System.out.println("The do-while");
+        System.out.println("do {");
+        System.out.println("  statement;");
+        System.out.println("} while (condition);");
+      }
+//      default -> // not necessary with do while loop ensuring case selection
+//        System.out.println("Selection not found.");
     }
   }
 }
